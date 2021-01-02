d4f873fdb82cd7ba40ccf8134199fb1a19793cbc
Julian Napolitan <julian@zumper.com>
2021-01-01 21:39:34 -0500

    initial commit

--- App.tsx
+++ App.tsx
@@ -1,21 +1,47 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+/**
+ * @author       julian@zumper.com (Julian Napolitan)
+ * @copyright    Copyright (c) 2020
+ */
 
-export default function App() {
+import firebase from 'firebase'
+import 'react-native-gesture-handler';
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import keys from './config/keys'
+import { DashboardScreen } from './DashboardScreen'
+import { HomeScreen } from './HomeScreen'
+import { LoadingScreen } from './LoadingScreen'
+import { SignInScreen } from './SignInScreen'
+import { SignUpScreen } from './SignUpScreen'
+
+const { Navigator, Screen } = createStackNavigator();
+
+export type RootStackParamList = {
+  Dashboard: undefined;
+  Home: undefined;
+  Loading: undefined;
+  'Sign In': undefined;
+  'Sign Up': undefined;
+};
+
+const App = () => {
+  if (!firebase.apps.length) {
+    firebase.initializeApp(keys.firebaseConfig);
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Navigator>
+        <Screen name="Home" component={HomeScreen} options={{ headerShown: false }} />
+        <Screen name="Sign In" component={SignInScreen} options={{ headerShown: false }} />
+        <Screen name="Sign Up" component={SignUpScreen} options={{ headerShown: false }} />
+        <Screen name="Dashboard" component={DashboardScreen} options={{ headerShown: false }} />
+        <Screen name="Loading" component={LoadingScreen} options={{ headerShown: false }} />
+      </Navigator>
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App
