fb64ef31e8705f66ebd42af36a6891d963bf4891
Julian Napolitan <julian@zumper.com>
2021-01-01 22:28:14 -0500

    add eslint

--- DashboardScreen.tsx
+++ DashboardScreen.tsx
@@ -3,12 +3,12 @@
  * @copyright    Copyright (c) 2020
  */
 
-import React, { useEffect, useState } from 'react';
-import { Button, StyleSheet, Text, View } from 'react-native';
-import { TouchableOpacity } from 'react-native-gesture-handler';
-import { StackNavigationProp } from '@react-navigation/stack';
+import React, { useEffect, useState } from 'react'
+import { Button, StyleSheet, Text, View } from 'react-native'
+import { TouchableOpacity } from 'react-native-gesture-handler'
+import { StackNavigationProp } from '@react-navigation/stack'
 
-import { signOut } from './api/userAuth';
+import { signOut } from './api/userAuth'
 import { RootStackParamList } from './App'
 import { getItem } from './utils/storage'
 
@@ -23,56 +23,56 @@ type Props = {
 };
 
 export const DashboardScreen = ({ navigation }: Props) => {
-  const [name, setName] = useState('')
-  const [exercised, setExercised] = useState(false)
-  const [drank, setDrank] = useState(false)
-  useEffect(() => {
-    const getUser = async () => {
-      console.log('GETTING USER FOR DASHBOARD')
-      const user = await getItem('@user')
-      console.log('USER GOT')
-      console.log('USER FROM DASHBOARD', user)
-      user && setName(user.displayName)
-    }
-    getUser()
-  }, [])
-  const handleSignOut = () => {
-    signOut()
-    navigation.navigate('Home')
-   }
-   return (
-     <View style={styles.container}>
-        <Text style={styles.text}>Enter your day, {name}!</Text>
-        <Text style={styles.questionText}>Did you exercise?</Text>
-        <Button title={exercised ? 'Yes' : 'No'} onPress={() => setExercised(!exercised)} />
-        <Text style={styles.questionText}>Did you drink?</Text>
-        <Button title={drank ? 'Yes' : 'No'} onPress={() => setDrank(!drank)} />
-        <TouchableOpacity style={styles.button} onPress={handleSignOut}>
-          <Text style={styles.text}>Sign Out</Text>
-        </TouchableOpacity>
-      </View>
-   )
- }
+	const [name, setName] = useState('')
+	const [exercised, setExercised] = useState(false)
+	const [drank, setDrank] = useState(false)
+	useEffect(() => {
+		const getUser = async () => {
+			console.log('GETTING USER FOR DASHBOARD')
+			const user = await getItem('@user')
+			console.log('USER GOT')
+			console.log('USER FROM DASHBOARD', user)
+			user && setName(user.displayName)
+		}
+		getUser()
+	}, [])
+	const handleSignOut = () => {
+		signOut()
+		navigation.navigate('Home')
+	}
+	return (
+		<View style={styles.container}>
+			<Text style={styles.text}>Enter your day, {name}!</Text>
+			<Text style={styles.questionText}>Did you exercise?</Text>
+			<Button title={exercised ? 'Yes' : 'No'} onPress={() => setExercised(!exercised)} />
+			<Text style={styles.questionText}>Did you drink?</Text>
+			<Button title={drank ? 'Yes' : 'No'} onPress={() => setDrank(!drank)} />
+			<TouchableOpacity style={styles.button} onPress={handleSignOut}>
+				<Text style={styles.text}>Sign Out</Text>
+			</TouchableOpacity>
+		</View>
+	)
+}
 
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  text: {
-    fontSize: 18,
-    fontWeight: 'bold',
-  },
-  questionText: {
-    marginTop: 30,
-  },
-  button: {
-    marginTop: 30,
-    width: "50%",
-    fontSize: 16,
-  },
+	container: {
+		flex: 1,
+		backgroundColor: '#fff',
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
+	text: {
+		fontSize: 18,
+		fontWeight: 'bold',
+	},
+	questionText: {
+		marginTop: 30,
+	},
+	button: {
+		marginTop: 30,
+		width: '50%',
+		fontSize: 16,
+	},
 })
 
--- HomeScreen.tsx
+++ HomeScreen.tsx
@@ -3,9 +3,9 @@
  * @copyright    Copyright (c) 2020
  */
 
-import React from 'react';
-import { Button, StyleSheet, Text, View } from 'react-native';
-import { StackNavigationProp } from '@react-navigation/stack';
+import React from 'react'
+import { Button, StyleSheet, Text, View } from 'react-native'
+import { StackNavigationProp } from '@react-navigation/stack'
 
 import { RootStackParamList } from './App'
 
@@ -18,33 +18,33 @@ type Props = {
   navigation: ProfileScreenNavigationProp;
 };
 
- export const HomeScreen = ({ navigation }: Props) => {
-   return (
-     <View style={styles.container}>
-        <Text>Welcome to Reset with Friends!</Text>
-        <Button
-          title="Sign In"
-          onPress={() =>
-            navigation.navigate('Sign In')
-          }
-        />
-        <Button
-          title="Sign Up"
-          onPress={() =>
-            navigation.navigate('Sign Up')
-          }
-        />
-      </View>
-   )
- }
+export const HomeScreen = ({ navigation }: Props) => {
+	return (
+		<View style={styles.container}>
+			<Text>Welcome to Reset with Friends!</Text>
+			<Button
+				title="Sign In"
+				onPress={() =>
+					navigation.navigate('Sign In')
+				}
+			/>
+			<Button
+				title="Sign Up"
+				onPress={() =>
+					navigation.navigate('Sign Up')
+				}
+			/>
+		</View>
+	)
+}
 
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: '#fff',
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
 })
 
